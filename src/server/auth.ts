72d5a94c8b7e99f95797608d37acdca138daecbc
Faizan Rasool <faizanraso@outlook.com>
2023-12-03 20:59:04 -0500

    push to main to deploy

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -24,11 +24,6 @@ declare module "next-auth" {
   // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
     session: ({ session, user }) => ({
@@ -45,15 +40,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.APPLE_CLIENT_ID,
       clientSecret: env.APPLE_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
