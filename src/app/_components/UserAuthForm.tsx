1dc1c317fbb73d9a04034ae217dbf45d8203f536
Faizan Rasool <faizanraso@outlook.com>
2023-12-05 21:32:17 -0500

    oauth only

--- src/app/_components/UserAuthForm.tsx
+++ src/app/_components/UserAuthForm.tsx
@@ -24,49 +24,71 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   }
 
   return (
-    <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-          </div>
-          <Button disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </Button>
+    <div
+      className={cn(
+        "flex h-full w-full flex-col rounded-xl bg-white p-10 sm:h-4/6 sm:w-4/6 md:w-3/6 lg:h-4/6 lg:w-3/6",
+      )}
+    >
+      <div className="justify-between">
+        <div className="py-3">
+          <p className="text-xl font-bold">🔒 locked in</p>
         </div>
-      </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
+        <div className="py-3">
+          <p className="text-md font-semibold">Log In</p>
+          <p className="text-xs text-gray-400">Continue to Locked In</p>
         </div>
       </div>
-      <Button variant="outline" type="button" disabled={isLoading}>
-        {isLoading ? (
-          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.gitHub className="mr-2 h-4 w-4" />
-        )}{" "}
-        Github
-      </Button>
+
+      <div className="flex h-full w-full flex-col justify-between py-3">
+        {/* <form onSubmit={onSubmit} className="flex flex-col"> */}
+        {/* <div className="flex flex-col">
+            <div className="py-2.5">
+              <Label className="" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                id="email"
+                placeholder="name@example.com"
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                disabled={isLoading}
+                className=""
+              />
+            </div>
+            <Button disabled={isLoading} className="font-medium">
+              {isLoading && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Continue with Email
+            </Button>
+          </div> */}
+
+        {/* <div className="relative py-2.5">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-sm lowercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or
+              </span>
+            </div>
+          </div> */}
+
+        {/* </form> */}
+
+        <Button variant="outline" type="button" disabled={isLoading}>
+          {isLoading ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <Icons.gitHub className="mr-2 h-4 w-4" />
+          )}{" "}
+          Github
+        </Button>
+
+        <div>test</div>
+      </div>
     </div>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -13,9 +13,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <main className="flex h-screen items-center justify-center bg-gradient-to-r from-cyan-400 to-blue-500">
-      <div className="flex h-full w-full items-center justify-center rounded-lg bg-white sm:h-4/6 sm:w-4/6 md:w-3/6 lg:h-4/6 lg:w-3/6">
-        <UserAuthForm className="w-4/6"/>
-      </div>
+      <UserAuthForm />
     </main>
   );
 }
