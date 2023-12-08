64402468bac9143aedd383705ef34a4bf85beb8b
Faizan Rasool <faizanraso@outlook.com>
2023-12-07 21:49:16 -0500

    login not working

--- src/app/_components/UserAuthForm.tsx
+++ src/app/_components/UserAuthForm.tsx
@@ -3,25 +3,23 @@
 import * as React from "react";
 import { Button } from "~/components/ui/button";
 import { Icons } from "~/components/ui/icons";
+import { signIn } from "next-auth/react";
 
 import { cn } from "~/lib/utils";
 
 export function UserAuthForm() {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  function signInWithGithub() {
+    signIn("github");
+  }
 
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-    setIsLoading(true);
-
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
+  function signInWithGoogle() {
+    signIn("google");
   }
 
   return (
     <div
       className={cn(
-        "relative z-10 m-auto h-[345px] w-[475px] flex-col items-center justify-center rounded-xl bg-white px-6 py-10 sm:px-10",
+        "relative z-10 m-auto h-fit w-[475px] flex-col items-center justify-center rounded-xl bg-white px-6 py-10 sm:px-10",
       )}
     >
       <div className="flex flex-col">
@@ -37,35 +35,36 @@ export function UserAuthForm() {
         </div>
       </div>
 
-      <div className="flex w-full flex-col gap-y-3 py-3">
-        <Button variant="outline" type="button" disabled={isLoading}>
-          {isLoading ? (
-            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-          )}{" "}
+      <div className="flex w-full flex-col gap-y-3 pb-6 pt-3">
+        <Button variant="outline" type="button" onClick={signInWithGithub}>
+          <Icons.gitHub className="mr-2 h-4 w-4" />
           Continue With Github
         </Button>
-        <Button variant="outline" type="button" disabled={isLoading}>
-          {isLoading ? (
-            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <Icons.google className="mr-2 h-4 w-4" />
-          )}{" "}
+        <Button variant="outline" type="button" onClick={signInWithGoogle}>
+          <Icons.google className="mr-2 h-4 w-4" />
           Continue With Google
         </Button>
       </div>
 
-      <div className="absolute bottom-0 flex flex-row gap-x-3 py-5">
-        <p className="text-xs font-medium text-gray-500 transition duration-150 hover:underline">
+      <div className="absolute bottom-0 flex flex-row gap-x-3 py-3">
+        <span
+          onClick={() => alert("test")}
+          className="cursor-pointer text-xs font-medium text-gray-500 transition duration-150 hover:underline"
+        >
           Help
-        </p>
-        <p className="text-xs font-medium text-gray-500 transition duration-150 hover:underline">
+        </span>
+        <span
+          onClick={() => alert("test")}
+          className="cursor-pointer text-xs font-medium text-gray-500 transition duration-150 hover:underline"
+        >
           Privacy
-        </p>
-        <p className="text-xs font-medium text-gray-500 transition duration-150 hover:underline">
+        </span>
+        <span
+          onClick={() => alert("test")}
+          className="cursor-pointer text-xs font-medium text-gray-500 transition duration-150 hover:underline"
+        >
           Terms
-        </p>
+        </span>
       </div>
     </div>
   );
--- src/env.js
+++ src/env.js
@@ -21,6 +21,10 @@ export const env = createEnv({
       (str) => process.env.VERCEL_URL ?? str,
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
   },
 
   client: {
@@ -31,6 +35,10 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
