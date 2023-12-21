6d6c0f3df0d483307a5d7c4dec31380cbf1e5b6c
Faizan Rasool <faizanraso@outlook.com>
2023-12-20 21:59:40 -0500

    prepare for home page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,19 @@
 import type { Metadata } from "next";
 
 import { UserAuthForm } from "../../components/user-auth-form";
+import { getServerAuthSession } from "~/server/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "locked-in",
   description: "Log in to locked-in",
 };
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const data = await getServerAuthSession();
+
+  if (data?.user) redirect("/home");
+
   return (
     <main className="absolute flex min-h-screen w-screen items-center justify-center overflow-hidden bg-black">
       <UserAuthForm />
