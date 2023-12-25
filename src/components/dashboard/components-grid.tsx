55118542ab97fb9067e4a82383b1040dac78fbf0
Faizan Rasool <faizanraso@outlook.com>
2023-12-24 22:47:20 -0500

    cards updated

--- src/components/dashboard/components-grid.tsx
+++ src/components/dashboard/components-grid.tsx
@@ -16,105 +16,180 @@ export default function ComponentsGrid() {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium text-neutral-200">
-              Total Revenue
+              Total Time "Locked-In"
             </CardTitle>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              stroke-width="1.5"
               viewBox="0 0 24 24"
               fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
+              xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4 text-muted-foreground text-neutral-400"
             >
-              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+              <path
+                d="M12 6L12 12L18 12"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold text-neutral-200">123 Hours</div>
+            <p className="text-xs text-muted-foreground text-neutral-400">
+              +25 hours in the past week
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium text-neutral-200">
+              Avg Session Length
+            </CardTitle>
+            <svg
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 text-muted-foreground text-neutral-400"
+            >
+              <path
+                d="M12 6L12 12L18 12"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
             </svg>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold text-neutral-200">
-              $45,231.89
+              54 minutes
             </div>
             <p className="text-xs text-muted-foreground text-neutral-400">
-              +20.1% from last month
+              +10 min in the past week
             </p>
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium text-neutral-200">
-              Subscriptions
+              Total Sessions
             </CardTitle>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
+              stroke-width="1.5"
               fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
+              xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4 text-muted-foreground text-neutral-400"
             >
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+              <path
+                d="M10 3L6 21"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              ></path>
+              <path
+                d="M20.5 16H2.5"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              ></path>
+              <path
+                d="M22 7H4"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              ></path>
+              <path
+                d="M18 3L14 21"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              ></path>
             </svg>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold text-neutral-200">+2350</div>
+            <div className="text-2xl font-bold text-neutral-200">52</div>
             <p className="text-xs text-muted-foreground text-neutral-400">
-              +180.1% from last month
+              +3 in the past week
             </p>
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium text-neutral-200">
-              Sales
+              Categories Tracked
             </CardTitle>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              stroke-width="1.5"
               viewBox="0 0 24 24"
               fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
+              xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4 text-muted-foreground text-neutral-400"
             >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
+              <path
+                d="M8 6L20 6"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M4 6.01L4.01 5.99889"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M4 12.01L4.01 11.9989"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M4 18.01L4.01 17.9989"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M8 12L20 12"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M8 18L20 18"
+                stroke="#a3a3a3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
             </svg>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold text-neutral-200">+12,234</div>
+            <div className="text-2xl font-bold text-neutral-200">4</div>
             <p className="text-xs text-muted-foreground text-neutral-400">
-              +19% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium text-neutral-200">
-              Active Now
-            </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground text-neutral-400"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold text-neutral-200">+573</div>
-            <p className="text-xs text-muted-foreground text-neutral-400">
-              +201 since last hour
+              +1 new category in the past week
             </p>
           </CardContent>
         </Card>
