65e8d58c6e18ad2086e1be5c55d4415b5fe78863
Faizan Rasool <faizanraso@outlook.com>
2024-01-03 21:03:30 -0500

    loading skeleton added

--- src/components/dashboard/category-list.tsx
+++ src/components/dashboard/category-list.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import { Button } from "../ui/button";
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { cn } from "~/lib/utils";
 
 interface CategoryListProps {
   categoriesData: any;
 }
 
 export default function CategoryList({ categoriesData }: CategoryListProps) {
-  if (!categoriesData) return;
+  // if (!categoriesData) return;
 
   const [breakdownPage, setBreakdownPage] = useState<number>(0);
-  const maxPage = Math.floor(categoriesData.length / 6);
+  const maxPage = Math.floor(categoriesData?.length / 6);
 
   function getNextPage() {
     setBreakdownPage(breakdownPage + 1);
@@ -24,30 +27,45 @@ export default function CategoryList({ categoriesData }: CategoryListProps) {
   return (
     <div className="flex h-full flex-col justify-between">
       <div className="space-y-7">
-        {categoriesData
-          ?.sort((a: any, b: any) => {
-            return b.hoursTracked - a.hoursTracked; // sort in desc order
-          })
-          .slice(breakdownPage * 6, breakdownPage * 6 + 6)
-          .map((item: any) => (
-            <div className="flex items-center" key={item.categoryName}>
-              <div className="space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  {item.categoryName}
-                </p>
+        {categoriesData ? (
+          categoriesData
+            ?.sort((a: any, b: any) => {
+              return b.hoursTracked - a.hoursTracked; // sort in desc order
+            })
+            .slice(breakdownPage * 6, breakdownPage * 6 + 6)
+            .map((item: any) => (
+              <div className="flex items-center" key={item.categoryName}>
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {item.categoryName}
+                  </p>
+                </div>
+                <div className="ml-auto font-medium">
+                  {item.hoursTracked} hours
+                </div>
               </div>
-              <div className="ml-auto font-medium">
-                {item.hoursTracked} hours
-              </div>
+            ))
+        ) : (
+          <SkeletonTheme baseColor="#202020" highlightColor="#444">
+            <div className="space-y-">
+              {Array.from({ length: 6 }, (_, index) => (
+                <div
+                  key={index}
+                  className={cn(index === 5 ? "text-xl" : "pb-5 text-xl")}
+                >
+                  <Skeleton />
+                </div>
+              ))}
             </div>
-          ))}
+          </SkeletonTheme>
+        )}
       </div>
       <div className="flex flex-row items-center justify-center space-x-2 ">
         <Button
           variant="outline"
           className="h-8 w-8 p-0"
           onClick={() => getPreviousPage()}
-          disabled={!breakdownPage}
+          disabled={!categoriesData || !breakdownPage}
         >
           <ChevronLeftIcon className="h-4 w-4" />
         </Button>
@@ -55,7 +73,7 @@ export default function CategoryList({ categoriesData }: CategoryListProps) {
           variant="outline"
           className="h-8 w-8 p-0"
           onClick={() => getNextPage()}
-          disabled={breakdownPage === maxPage}
+          disabled={!categoriesData || breakdownPage === maxPage}
         >
           <ChevronRightIcon className="h-4 w-4" />
         </Button>
