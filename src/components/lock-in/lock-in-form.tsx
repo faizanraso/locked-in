a041720adaa82c3757954e284207a54a3e681df7
Faizan Rasool <faizanraso@outlook.com>
2024-01-07 12:12:05 -0500

    timer done

--- src/components/lock-in/lock-in-form.tsx
+++ src/components/lock-in/lock-in-form.tsx
@@ -1,17 +1,56 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Button } from "~/components/ui/button";
 import { CategoriesCombobox } from "./categories-combobox";
 
 export default function LockInForm() {
+  let timeInterval = useRef<any>(null);
+  const [timer, setTimer] = useState<number>(0);
+  const [timerDisplayText, setTimerDisplayText] = useState<string>("00:00:00");
   const [isSessionActive, setIsSessionActive] = useState<boolean>(false);
+
+  function handleStart() {
+    if (isSessionActive) return;
+    setIsSessionActive(true);
+    timeInterval.current = setInterval(() => {
+      setTimer((prev) => prev + 1000);
+    }, 1000);
+  }
+
+  function handleStop() {
+    if (!isSessionActive) return;
+    setIsSessionActive(false);
+    clearInterval(timeInterval.current);
+    setTimer(0);
+  }
+
+  useEffect(() => {
+    getTimerDisplayText();
+  }, [timer]);
+
+  function getTimerDisplayText() {
+    let seconds = Math.floor(timer / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+
+    setTimerDisplayText(
+      `${hours.toString().padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`,
+    );
+  }
 
   return (
     <section className="flex flex-col items-center justify-center gap-y-3 rounded-lg border border-neutral-800 p-8">
       <div className="w-[300px] items-center rounded-lg border border-neutral-800 bg-black px-12 py-2 text-center">
-        <span className="min-w-96 text-3xl text-neutral-100">00:00:00</span>
+        <span className="min-w-96 text-3xl text-neutral-100">
+          {timerDisplayText}
+        </span>
       </div>
       <div className="flex flex-row py-2">
         <CategoriesCombobox />
@@ -23,12 +62,14 @@ export default function LockInForm() {
         <Button
           className="w-[300px] bg-neutral-100 hover:bg-neutral-300"
           disabled={isSessionActive}
+          onClick={handleStart}
         >
           Start Session
         </Button>
         <Button
           className="w-[300px] bg-red-700 text-neutral-100 hover:bg-red-900"
           disabled={!isSessionActive}
+          onClick={handleStop}
         >
           Stop Session
         </Button>
