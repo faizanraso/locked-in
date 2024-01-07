b335df86e37cbb023078fac4024ab140332996ce
Faizan Rasool <faizanraso@outlook.com>
2024-01-06 21:00:56 -0500

    ui improvment

--- src/components/lock-in/categories-combobox.tsx
+++ src/components/lock-in/categories-combobox.tsx
@@ -53,16 +53,16 @@ export function CategoriesCombobox() {
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-[250px] justify-between text-neutral-100"
         >
           {value
             ? frameworks.find((framework) => framework.value === value)?.label
             : "Select framework..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 text-neutral-100 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
+      <PopoverContent className="w-[250px] p-0 text-neutral-100">
+        <Command className="">
           <CommandInput placeholder="Search framework..." />
           <CommandEmpty>No framework found.</CommandEmpty>
           <CommandGroup>
--- src/components/lock-in/lock-in-form.tsx
+++ src/components/lock-in/lock-in-form.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { CategoriesCombobox } from "./categories-combobox";
+import { Button } from "~/components/ui/button";
 
 export default function LockInForm() {
   return (
-    <section className="flex flex-col items-center justify-center gap-y-3 w-full bg-white">
-      <div className="rounded-lg bg-neutral-900 px-12 py-4 w-[300px]">
-        <span className="min-w-96 text-3xl">1:02:32</span>
+    <section className="flex w-full flex-col items-center justify-center gap-y-3">
+      <div className="w-[300px] rounded-lg border border-neutral-800 bg-black px-12 py-4 text-center items-center">
+        <span className="min-w-96 text-3xl text-neutral-100">1:02:32</span>
       </div>
-      <div className="">
+      <div className="flex flex-row">
         <CategoriesCombobox />
+        <Button className="ml-[10px] w-[40px] rounded-full border border-neutral-800 bg-black text-neutral-100 hover:bg-neutral-800">
+          +
+        </Button>
       </div>
     </section>
   );
