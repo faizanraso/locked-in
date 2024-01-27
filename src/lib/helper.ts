4faf998dee724b3c5145d0b871b6a76392d90939
Faizan Rasool <faizanraso@outlook.com>
2024-01-27 11:27:20 -0500

    done

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,17 +1,18 @@
 export function getTimerDisplayText(timer: number) {
   const seconds = Math.floor(timer / 1000) % 60;
-  const minutes = Math.floor(seconds / 60) % 60;
-  const hours = Math.floor(minutes / 60);
+  const minutes = Math.floor(timer / 1000 / 60) % 60;
+  const hours = Math.floor(timer / 1000 / 60 / 60);
 
   return `${hours.toString().padStart(2, "0")}:${minutes
     .toString()
     .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
 }
 
 export function displayConvertedTime(duration: number) {
   // This function is used to convert ms to an appropriate unit of measurement (secs, mins or hours)
   if (duration < 60000) {
-    return `${(duration / 1000)} secs`;
+    return `${duration / 1000} secs`;
   } else if (duration < 3600000) {
     return `${(duration / 60000).toFixed(2)} mins`;
   } else {
