bcdb43881de6f23e34f354e9d82c02e3155c82e0
Faizan Rasool <faizanraso@outlook.com>
2024-01-28 09:39:36 -0500

    squiggly lines added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Header from "~/components/landing-page/header";
+import SquigglyLines from "~/components/other/squiqqly-lines";
 
 export default function BackgroundBoxesPreview() {
   return (
@@ -10,7 +11,11 @@ export default function BackgroundBoxesPreview() {
         <div className="py-28">
           <div className="flex px-10">
             <h1 className="inline-block bg-gradient-to-b from-neutral-200 via-zinc-300 to-stone-500 bg-clip-text text-center text-4xl font-bold text-transparent">
-              A Better Way to Track Your Time.
+              A Better Way to Track{" "}
+              <span className="relative whitespace-nowrap text-sky-600">
+                <SquigglyLines />
+                <span className="relative">Your Time.</span>
+              </span>{" "}
             </h1>
           </div>
         </div>
